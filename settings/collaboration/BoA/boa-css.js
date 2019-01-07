23be5fc46b072e5e6a503f54128381a6329ceb8e
Eugene Louie <eugene_louie@us.ibm.com>
2019-01-07 09:58:55 -0500

    Initial Tests

---
+++ settings/collaboration/BoA/boa-css.js
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+if (typeof (dojo) != 'undefined'){
+	dojo.place("<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/BoA/custom.css?repoName=BlueCross\"</link>",
+	dojo.doc.head,"last"
+	);
+}
